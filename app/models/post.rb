41d2d40ef5d2f2165a730f7f6eae8a8e65a58043
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-09 13:29:20 +0300

    Create update posts counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
