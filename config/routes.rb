839dbbdda5e96ba97c247f4ccc307de6308e8970
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-14 16:32:29 +0300

    Refactor the routes to use the resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # users
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-
-  # posts
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 end
