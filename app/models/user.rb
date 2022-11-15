afbb1e7867f7a81b19e314390d052530b001e184
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-15 18:40:47 +0300

    Display all users and their posts count dynamically

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ApplicationRecord
   def recent_three
     posts.order(created_at: :desc).limit(3)
   end
-
   # validation
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_initialize :init
+
+  def init
+    self.posts_counter ||= 0
+  end
 end
