ab5a85d4c90c07a40de561cfa2b4995747ddf58d
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-16 09:50:55 +0300

    Display a post for a given user dynamically

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,12 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @post_text = @post.text
+    @author = @post.author.name
+    @comments = @post.comments
+    @comments_count = @post.comments.length
+    @likes_count = @post.likes.length
+  end
 end
