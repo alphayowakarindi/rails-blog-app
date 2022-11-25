e0ed9b4df2c30b92b0bf0a5f6261dde5f0102f2a
Mark Otuya <markotuya0@gmail.com>
2022-11-25 11:32:59 +0100

    give user ability to create and destroy a post

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,10 @@ class Ability
 
     can :read, :all
     can :destroy, Post, author_id: user.id
-
+    # comments
+    can :create, Post
+    can :create, Comment
+    can :destroy, Comment, author_id: user.id
     return unless user.role?
 
     can :manage, :all
